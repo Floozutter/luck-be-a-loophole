3c1e7a5fb17f30dec6d35a62de5b8a3cb2b1fc54
Floozutter <floozutter@gmail.com>
2021-03-19 06:04:16 -0700

    use package structure

---
+++ luckhole/__main__.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+def parse_args() -> str:
+    parser = ArgumentParser(description = __doc__)
+    parser.add_argument(
+        "savefile",
+        type = str,
+        help = "path to savefile, such as \"LBAL.save\""
+    )
+    return parser.parse_args().savefile
+
+def main(savefile: str) -> None:
+    with open(savefile, "r+") as file:
+        print(file.read())
+
+if __name__ == "__main__":
+    main(parse_args())

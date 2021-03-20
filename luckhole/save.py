73209c4af4e47a9beb2962b78a400b1be7e86e50
Floozutter <floozutter@gmail.com>
2021-03-19 21:34:51 -0700

    add Save.coins property

--- luckhole/save.py
+++ luckhole/save.py
@@ -5,3 +5,9 @@ class Save:
         self.data = tuple(map(loads, text.strip().split("\n")))
     def __str__(self) -> str:
         return "\n".join(map(dumps, self.data))
+    @property
+    def coins(self) -> int:
+        return int(self.data[2]["coins"])
+    @coins.setter
+    def coins(self, value: int) -> None:
+        self.data[2]["coins"] = value

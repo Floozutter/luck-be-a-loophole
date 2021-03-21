0d0db72d71e5e178b0d1463d9f21b59426179b0f
Floozutter <floozutter@gmail.com>
2021-03-21 12:16:53 -0700

    add prompt helper

--- luckhole/__main__.py
+++ luckhole/__main__.py
@@ -4,6 +4,7 @@ a save editor for Luck Be a Landlord!
 
 from . import Save
 from argparse import ArgumentParser
+from typing import Callable, Optional
 
 def parse_args() -> tuple[str, str]:
     parser = ArgumentParser(description = __doc__)
@@ -20,11 +21,23 @@ def parse_args() -> tuple[str, str]:
     args = parser.parse_args()
     return args.insavefile, args.outsavefile
 
+def prompt(
+    name: str,
+    default: Optional[str] = None,
+    valid: Callable[[str], bool] = lambda s: True
+) -> str:
+    text = f"{name} [{default}]: " if default is not None else f"{name}: "
+    while True:
+        value = input(text)
+        if default is not None and not value:
+            return default
+        elif valid(value):
+            return value
+
 def main(insavefile: str, outsavefile: str) -> None:
     with open(insavefile, "r") as ifile:
         save = Save(ifile.read())
-    while (lambda s: not s.isdigit() and s)(coins := input(f"coins [{save.coins}]: ")): pass
-    save.coins = int(coins) if coins else save.coins
+    save.coins = int(prompt("coins", str(save.coins), lambda s: s.isdigit()))
     with open(outsavefile, "w") as ofile:
         ofile.write(str(save).strip() + "\n")
 
